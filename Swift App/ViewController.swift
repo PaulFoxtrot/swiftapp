030986c2b398a316fc9927891b6de14519d1938a
PaulFoxtrot <foxtrot@pdnk.org>
2017-03-22 22:24:45 +0700

    can get the sum of textfields by pressing the button

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var textFieldOne: UITextField!
+    
+    @IBOutlet weak var textFieldTwo: UITextField!
+    
+    @IBOutlet weak var theLabel: UILabel!
+    
+    @IBAction func firstButton(_ sender: Any) {
+        theLabel.text = "Answer is \(Double(textFieldOne.text!)! + Double(textFieldTwo.text!)!)"
+    }
+    
+    @IBAction func secondButton(_ sender: Any) {
+
+    }
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+
+        self.view.backgroundColor = UIColor.cyan.withAlphaComponent(4.4)
     }
 
+
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
